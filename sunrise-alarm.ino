5cbc684e262737b2868e0decb09ee177b3820992
Michael Murdock <m.a.murdock@outlook.com>
2026-01-07 10:31:06 -0500

    basic helper functions for managing RTC and screen

--- sunrise-alarm.ino
+++ sunrise-alarm.ino
@@ -4,6 +4,7 @@
 #include <Wire.h>             // two-wire (I2C) communication
 
 // Global variables shared between things:
+DateTime dailyAlarmTime = DateTime(2000, 1, 1, 6, 0, 0);
 // Alarm currently active
 // Alarm enabled / disabled
 // Lights turned on / off
@@ -11,7 +12,7 @@
 
 
 // #########################################################################
-// Display management and text printing
+// Display  and text printing
 
 constexpr uint8_t SCREEN_WIDTH      = 128;   // pixels
 constexpr uint8_t SCREEN_HEIGHT     = 32;    // pixels
@@ -26,8 +27,18 @@ void oledSetup() { // Initial setup of the OLED screen
   display.display();
 }
 
-constexpr uint8_t TEXT_HALF_HEIGHT  = 1;  // 8 pixels height  = 1/4 screen height
-constexpr uint8_t TEXT_QRTR_HEIGHT  = 2;  // 16 pixels height = 1/2 screen height
+constexpr uint8_t TEXT_QRTR_HEIGHT  = 1;  // 8 pixels height  = 1/4 screen height
+constexpr uint8_t TEXT_HALF_HEIGHT  = 2;  // 16 pixels height = 1/2 screen height
+constexpr uint8_t TEXT_3QRTR_HEIGHT = 3;  // 24 pixels height = 3/4 screen height
+constexpr uint8_t TEXT_FULL_HEIGHT  = 4;  // 32 pixels height = total screen height
+
+void clearDisplay() { // First step before putting new things on the display
+  display.clearDisplay();
+}
+
+void refreshDisplay() {
+  display.display();
+}
 
 // enum TextSize = {TEXT_SMALL, TEXT_MEDIUM, TEXT_LARGE};
 // void displayText(char* text, TextSize size) {
@@ -37,21 +48,99 @@ constexpr uint8_t TEXT_QRTR_HEIGHT  = 2;  // 16 pixels height = 1/2 screen heigh
 
 
 // #########################################################################
-// RTC clock / alarm management
+// RTC clock / alarm 
+
+// Setup for using Arduino interrupt mechanism to wake it from sleep when an
+// alarm goes off. Currently this is not really used, because I don't want to
+// put the Arduino to sleep if the *only* way to wake it up is with an alarm.
+// I think if I want to use this "sleep" functionality, I should also attach
+// a button to the D2 pin and allow the button to wake it up. But honestly, 
+// this all might be overkill. Does it really need to sleep if it is going to 
+// be plugged in all the time?
+// #define CLOCK_INTERRUPT_PIN 2
+// attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), wakeISR, FALLING);
 
 RTC_DS3231 rtc;
 
-void rtcSetup() {
+void rtcSetup() {   // Should be run once at Arduino reboot
+  Wire.begin();
+  if (!rtc.begin()) {
+    // TODO: handle an error where the RTC is not found / not connected
+  }
 
+  rtc.writeSqwPinMode(DS3231_OFF);  // disable SQW alarm interrupt
+  rtc.disable32K();                 // 32K consumes power and isn't needed
+
+  // Alarms should be disabled because they may have been flagged while Arduino
+  // was powered off and could trigger immediately
+  rtc.disableAlarm(1);  
+  rtc.disableAlarm(2);
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+
+  // Since alarm times cannot be queried from the RTC, they need to be reset
+  // if the Arduino was rebooted
+  rtc.setAlarm2(dailyAlarmTime, DS3231_A2_Hour);
+}
+
+bool clockLostPower() {
+  // Detects if the RTC power was lost (battery lost its charge)
+  // Could be used in the future to prompt for new time/date entry
+  return rtc.lostPower();
+}
+
+void setClockTime(const DateTime &newTime) {
+  DateTime now = rtc.now();
+  rtc.adjust(DateTime(
+    now.year(), now.month(), now.day(),
+    newTime.hour(), newTime.minute(), newTime.second()
+  ));
+}
+
+void setClockDate(const DateTime &newDate) {
+  DateTime now = rtc.now();
+  rtc.adjust(DateTime(
+    newDate.year(), newDate.month(), newDate.day(),
+    now.hour(), now.minute(), now.second()
+  ));
+}
+
+void setPreciseAlarm(DateTime alarmTime) {  // Alarm 1 has seconds-level precision
+  rtc.clearAlarm(1);
+  rtc.setAlarm1(
+    DateTime(0, 0, 0, alarmTime.hour(), alarmTime.minute(), 0), 
+    DS3231_A1_Hour
+  );
+}
+
+void setDailyAlarm(int hour, int minute) {  //Alarm
+  dailyAlarmTime = DateTime(2000, 1, 1, hour, minute, 0);
+  rtc.clearAlarm(2);
+  rtc.setAlarm2(dailyAlarmTime, DS3231_A2_Hour);
+}
+
+bool isAlarm1Triggered() {
+  if (rtc.alarmFired(1)) {
+    rtc.clearAlarm(1);
+    return true;
+  }
+  return false;
+}
+
+bool isAlarm2Triggered() {
+  if (rtc.alarmFired(2)) {
+    rtc.clearAlarm(2);
+    return true;
+  }
+  return false;
 }
 
 
 // #########################################################################
-// Button management
+// Buttons
 
 // Pin definitions
 #define LED_STRIP           6
-#define CLOCK_INTERRUPT_PIN 2
 #define ON_OFF_BUTTON       A7
 #define DISPLAY_ON_OFF_BTN  A6
 #define DECREASE_BUTTON     A2
@@ -72,13 +161,17 @@ bool isButtonPressed(int button_pin) {
 
 
 // #########################################################################
-// LED strip management
+// LED strip 
 
 
 
 
 // #########################################################################
 // State management, main dispatcher functions
+
+// Some probably-shared variables:
+// Is an alarm active? What time was it started?
+// Is the screen turned on or off? That one gets set by a button
 
 
 
@@ -89,10 +182,11 @@ bool isButtonPressed(int button_pin) {
 void setup() {
   Serial.begin(9600); // open serial communication
   oledSetup();
+  rtcSetup();
 }
 
 void loop() {
-  // Listen for a button click / event
+  // Listen for a button click / event / alarm going off
   // Handle any events / button clicks 
   //    -> set variables
   //    -> change the device state
@@ -101,7 +195,7 @@ void loop() {
   //    e.g. lights on if alarm on, or they're turned on manually
 
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setCursor(0, 16);
   display.println("Hello");
   display.display();
