f5d37a3822ca9ac72fec693030ed9962c34c3c94
Michael Murdock <m.a.murdock@outlook.com>
2026-01-10 20:06:10 -0500

    Outline of state and button functionality. A few other changes.

--- sunrise-alarm.ino
+++ sunrise-alarm.ino
@@ -5,13 +5,15 @@
 
 // Global variables shared between things:
 DateTime dailyAlarmTime = DateTime(2000, 1, 1, 6, 0, 0);
-// Alarm currently active
+bool dailyAlarmActive = false;
+bool screenOn = true;
+
 // Alarm enabled / disabled
 // Lights turned on / off
 // Light brightness percentage
 
 
-// #########################################################################
+// ######################################################################################
 // Display  and text printing
 
 constexpr uint8_t SCREEN_WIDTH      = 128;   // pixels
@@ -47,16 +49,15 @@ void refreshDisplay() {
 // }
 
 
-// #########################################################################
+// ######################################################################################
 // RTC clock / alarm 
 
-// Setup for using Arduino interrupt mechanism to wake it from sleep when an
-// alarm goes off. Currently this is not really used, because I don't want to
-// put the Arduino to sleep if the *only* way to wake it up is with an alarm.
-// I think if I want to use this "sleep" functionality, I should also attach
-// a button to the D2 pin and allow the button to wake it up. But honestly, 
-// this all might be overkill. Does it really need to sleep if it is going to 
-// be plugged in all the time?
+// Setup for using Arduino interrupt mechanism to wake it from sleep when an alarm goes 
+// off. Currently this is not really used, because I don't want to put the Arduino to 
+// sleep if the *only* way to wake it up is with an alarm. I think if I want to use this 
+// "sleep" functionality, I should also attach a button to the D2 pin and allow the 
+// button to wake it up. But honestly, this all might be overkill. Does it really need 
+// to sleep if it is going to be plugged in all the time?
 // #define CLOCK_INTERRUPT_PIN 2
 // attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), wakeISR, FALLING);
 
@@ -119,7 +120,7 @@ void setDailyAlarm(int hour, int minute) {  //Alarm
   rtc.setAlarm2(dailyAlarmTime, DS3231_A2_Hour);
 }
 
-bool isAlarm1Triggered() {
+bool isPreciseAlarmTriggered() {
   if (rtc.alarmFired(1)) {
     rtc.clearAlarm(1);
     return true;
@@ -127,7 +128,7 @@ bool isAlarm1Triggered() {
   return false;
 }
 
-bool isAlarm2Triggered() {
+bool isDailyAlarmTriggered() {
   if (rtc.alarmFired(2)) {
     rtc.clearAlarm(2);
     return true;
@@ -135,20 +136,84 @@ bool isAlarm2Triggered() {
   return false;
 }
 
+void disableDailyAlarm() {
+  rtc.disableAlarm(2);
+}
 
-// #########################################################################
+void enableDailyAlarm() {
+  rtc.clearAlarm(2);
+  rtc.setAlarm2(dailyAlarmTime, DS3231_A2_Hour);
+}
+
+void checkAlarm() {
+  if (isDailyAlarmTriggered()) {
+    dailyAlarmActive = true;
+    // TODO: wake up screen
+  }
+}
+
+
+// ######################################################################################
+// LED strip 
+
+
+
+
+// ######################################################################################
+// State management, main dispatcher functions
+
+// Some probably-shared variables:
+  // Is an alarm active? What time was it started?
+  // Is the screen turned on or off? That one gets set by a button
+
+// Buttons
+  // SCREEN_ON_OFF_BUTTON
+    // Turns the screen on or off
+    // When the screen is off no other buttons do anything
+    // An alarm can still be triggered, however. This also turns the screen back on.
+  // LED_ON_OFF_BUTTON
+    // Turns the LEDs on or off
+    // If an alarm is active (that's why the LEDs are on) de-activate the alarm
+    // Immediately switch into or out of "LEDs on" state on the screen
+      // If switching out of that state, always return to "home screen" (display clock)
+    // Also functions as "secret" method of getting into "Set clock" mode
+      // If pressed while the alarm is being set, save alarm and move to set clock mode
+      // If pressed in set clock mode, save clock and move to set home screen
+  // DECREASE_BUTTON and INCREASE_BUTTON
+    // Increase or decrease a current numeric value (setting alarm or clock)
+    // Increase or decrease the LED brightness
+      // If in LEDs on state and alarm is active, change how many "minutes into" the alarm it is, which changes the brightness
+    // If in "display clock" state, enable or disable the daily alarm
+    // Values wrap as appropriate (59 minutes goes back around to 0 minutes)
+  // MODE_BUTTON
+    // Cycle from home (display time) -> set alarm -> set alarm duration
+      // If in set clock mode, save clock and move to home screen
+    // When exiting, moving from one state to another, save what was just set
+
+// States:
+  // Display clock
+  // Set alarm
+  // Set alarm duration
+  // LEDs on (including alarm on)
+    // Display brightness percentage of LEDs
+    // Display alarm activation time if an alarm is currently active
+    // Display minutes until full brightness if an alarm is currently active
+
+
+
+// ######################################################################################
 // Buttons
 
 // Pin definitions
 #define LED_STRIP           6
-#define ON_OFF_BUTTON       A7
-#define DISPLAY_ON_OFF_BTN  A6
+#define SCREEN_ON_OFF_BTN   A6
+#define LED_ON_OFF_BUTTON   A7
 #define DECREASE_BUTTON     A2
 #define INCREASE_BUTTON     A0
 #define MODE_BUTTON         A3
-#define ALM_ENABLE_SWITCH   A1
+// #define ALM_ENABLE_SWITCH   A1
 
-#define BUTTON_THRESHOLD    2   // out of 5V
+constexpr float buttonVoltageThreshold = 2;   // out of 5V
 
 float getPinVoltage(int pin) {
   // Read the analog voltage (0-5V) from an analog input pin
@@ -156,27 +221,36 @@ float getPinVoltage(int pin) {
 }
 
 bool isButtonPressed(int button_pin) {
-  return getPinVoltage(button_pin) > BUTTON_THRESHOLD;
+  return getPinVoltage(button_pin) > buttonVoltageThreshold;
+}
+
+// Most recent times each of the buttons were pressed
+constexpr unsigned long doublePressTime_ms = 200;
+unsigned long lastPress_ScreenOnOffButton = 0;
+unsigned long lastPress_LEDOnOffButton = 0;
+unsigned long lastPress_DecreaseButton = 0;
+unsigned long lastPress_IncreaseButton = 0;
+unsigned long lastPress_ModeButton = 0;
+
+void handleButtonPresses() {
+  // run through the buttons
+    // compare current time to last time pressed
+    // determine if button is "validly" pressed. If so:
+      // Update "last time pressed"
+      // Call method that handles this button being pressed (in State management section)
+
+  if (isButtonPressed(SCREEN_ON_OFF_BTN) && millis() - lastPress_ScreenOnOffButton > doublePressTime_ms) {
+    lastPress_ScreenOnOffButton = millis();
+    // Do button stuff (function that is called in the "state management" area?)
+  }
+  if (isButtonPressed(LED_ON_OFF_BUTTON) && millis() - lastPress_LEDOnOffButton > doublePressTime_ms) {
+    lastPress_LEDOnOffButton = millis();
+    // Do button stuff
+  }
 }
 
 
-// #########################################################################
-// LED strip 
-
-
-
-
-// #########################################################################
-// State management, main dispatcher functions
-
-// Some probably-shared variables:
-// Is an alarm active? What time was it started?
-// Is the screen turned on or off? That one gets set by a button
-
-
-
-
-// #########################################################################
+// ######################################################################################
 // MAIN SETUP AND LOOP
 
 void setup() {
@@ -187,6 +261,8 @@ void setup() {
 
 void loop() {
   // Listen for a button click / event / alarm going off
+  checkAlarm();
+  handleButtonPresses();
   // Handle any events / button clicks 
   //    -> set variables
   //    -> change the device state
@@ -200,9 +276,9 @@ void loop() {
   display.println("Hello");
   display.display();
 
-  if (isButtonPressed(ALM_ENABLE_SWITCH)) {
-    Serial.println("ALM_ENABLE_SWITCH on");
-  }
+  // if (isButtonPressed(ALM_ENABLE_SWITCH)) {
+  //   Serial.println("ALM_ENABLE_SWITCH on");
+  // }
 
   delay(100);
 }
